d2b168e53d29a8ce1770d5c9eccd4c31fcadf917
TomGarfild <safronuyk.allex73@gmail.com>
2022-10-22 13:52:45 +0300

    Lab5

--- DistributedComputing/Lab5/Lab5_b/Program.cs
+++ DistributedComputing/Lab5/Lab5_b/Program.cs
@@ -1,7 +1,15 @@
 ﻿using System.Text;
 
-const int count = 3;
+const int count = 10;
 const string characters = "ABCD";
+
+Dictionary<char, char> replacable = new Dictionary<char, char>()
+{
+    {'A', 'C'},
+    {'C', 'A'},
+    {'B', 'D'},
+    {'D', 'B'}
+};
 
 var values = new StringBuilder[4];
 for (var i = 0; i < values.Length; i++)
@@ -13,11 +21,7 @@ var success = false;
 
 var barrier = new Barrier(4, (b) =>
 {
-    if (b.CurrentPhaseNumber == 500000)
-    {
-        Console.WriteLine("Oh fuck");
-    }
-    Console.WriteLine($"Phase action, phase={b.CurrentPhaseNumber}");
+    Console.WriteLine($"Step={b.CurrentPhaseNumber}");
     var groups = values.Select(v => v.ToString().Count(c => c is 'A' or 'B')).GroupBy(v => v);
     foreach (var group in groups)
     {
@@ -34,37 +38,44 @@ var barrier = new Barrier(4, (b) =>
 
 void Work(StringBuilder sb)
 {
-//    while (!success)
-//    {
-//        for (var i = 0; i < sb.Length; i++)
-//        {
-//            if (success)
-//            {
-//                break;
-//            }
+    while (!success)
+    {
 
-//            sb[i] = (char)('A' + (sb[i] - 'A' + 2) % 4);
-//            barrier.SignalAndWait();
-//        }
-//    }
+        if (success)
+        {
+            break;
+        }
+        
+        var kv = replacable.ElementAt(Random.Shared.Next(replacable.Count));
 
         for (var i = 0; i < sb.Length; i++)
         {
-            if (success)
+            if (sb[i] == kv.Key)
             {
-                break;
+                sb[i] = kv.Value;
             }
-
-            if (sb[i] == 'C')
-            {
-                sb[i] = 'A';
-            }
-            else if (sb[i] == 'D')
-            {
-                sb[i] = 'B';
-            }
-            barrier.SignalAndWait();
         }
+
+        // sb[i] = (char)('A' + (sb[i] - 'A' + 2) % 4);
+        barrier.SignalAndWait();
+    }
+    //for (var i = 0; i < sb.Length; i++)
+    //{
+    //    if (success)
+    //    {
+    //        break;
+    //    }
+
+    //    if (sb[i] == 'C')
+    //    {
+    //        sb[i] = 'A';
+    //    }
+    //    else if (sb[i] == 'D')
+    //    {
+    //        sb[i] = 'B';
+    //    }
+    //    barrier.SignalAndWait();
+    //}
 }
 
 Parallel.ForEach(values, Work);
--- DistributedComputing/Lab5/Lab5_c/main.go
+++ DistributedComputing/Lab5/Lab5_c/main.go
@@ -1,8 +1,130 @@
 package main
 
-func run(semaphore chan []int) {
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
 
+const ArraysCount = 3
+const ArraySize = 10
+const UpperBound = 5
+
+var random = rand.New(rand.NewSource(time.Now().Unix()))
+
+type ArrayGroup struct {
+	arrays [][]int
+	sync.WaitGroup
+}
+
+func createArrayGroup(arraysCount, arraySize, upperBound int) *ArrayGroup {
+	return &ArrayGroup{arrays: initArrayGroup(arraysCount, arraySize, upperBound)}
+}
+
+func initArrayGroup(arraysCount, arraySize, upperBound int) [][]int {
+	arrayList := make([][]int, arraysCount, arraySize)
+
+	for i := 0; i < arraysCount; i++ {
+		arrayList[i] = generateArray(arraySize, upperBound)
+	}
+
+	return arrayList
+}
+
+func generateArray(size, upperBound int) []int {
+	array := make([]int, size)
+
+	for i := 0; i < size; i++ {
+		array[i] = random.Intn(upperBound)
+	}
+
+	return array
+}
+
+func printArrayGroup(arrayGroup *ArrayGroup) {
+	for i := 0; i < len(arrayGroup.arrays); i++ {
+		fmt.Println(arrayGroup.arrays[i])
+	}
+
+	fmt.Println()
+}
+
+func run(arrayGroup *ArrayGroup, waitGroup *sync.WaitGroup, arraysCount, arraySize, upperBound int) {
+	stop := false
+
+	for !stop {
+		waitGroup.Add(arraysCount)
+
+		for i := 0; i < arraysCount; i++ {
+			go changeElement(arrayGroup, waitGroup, i, arraySize, upperBound)
+		}
+
+		waitGroup.Wait()
+
+		if checkRule(arrayGroup, arraysCount) {
+			stop = true
+			fmt.Println("ArrayGroup sum are equal. Terminate...")
+		}
+
+		printArrayGroup(arrayGroup)
+	}
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func changeElement(arrayGroup *ArrayGroup, waitGroup *sync.WaitGroup, currentArrayIndex, arraySize, upperBound int) {
+	elemToChange := random.Intn(arraySize)
+	sign := 1
+
+	if random.Intn(2) == 1 {
+		sign = -1
+	}
+
+	if Abs(arrayGroup.arrays[currentArrayIndex][elemToChange]) < upperBound {
+		arrayGroup.arrays[currentArrayIndex][elemToChange] += sign
+	}
+
+	time.Sleep(100 * time.Millisecond)
+
+	waitGroup.Done()
+}
+
+func checkRule(arrayGroup *ArrayGroup, arraysCount int) bool {
+	sumArray := make([]int, arraysCount)
+
+	for i := 0; i < arraysCount; i++ {
+		currentArraySum := 0
+
+		for _, j := range arrayGroup.arrays[i] {
+			currentArraySum += j
+		}
+
+		sumArray[i] = currentArraySum
+	}
+
+	return checkSumEquality(sumArray)
+}
+
+func checkSumEquality(array []int) bool {
+	fmt.Println("Sums: ", array)
+
+	for i := range array {
+		if array[0] != array[i] {
+			return false
+		}
+	}
+
+	return true
 }
 
 func main() {
+	arrayGroup := createArrayGroup(ArraysCount, ArraySize, UpperBound)
+	waitGroup := new(sync.WaitGroup)
+	run(arrayGroup, waitGroup, ArraysCount, ArraySize, UpperBound)
 }
