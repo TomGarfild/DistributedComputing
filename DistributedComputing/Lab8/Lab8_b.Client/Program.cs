9d1e5749d230ed5a58406d5f330b76f3e2d05027
TomGarfild <safronuyk.allex73@gmail.com>
2022-12-16 21:08:14 +0200

    Lab8.b

---
+++ DistributedComputing/Lab8/Lab8_b.Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Net.Client;
+
+namespace Lab8_b.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:7158");
+            var client = new Studio.StudioClient(channel);
+
+
+            var eminemId = client.AddArtist(new AddArtistRequest() { Name = "Eminem" });
+            Console.WriteLine($"Eminem: {eminemId.Id}");
+            var nfId = client.AddArtist(new AddArtistRequest() { Name = "NF" });
+            Console.WriteLine($"NF: {nfId.Id}");
+
+            Console.WriteLine(client.GetArtists(new GetArtistsRequest()).Ids);
+
+            client.DeleteArtist(new DeleteArtistRequest() { Id = nfId.Id });
+            client.UpdateArtist(new UpdateArtistRequest() { Id = eminemId.Id, Name = "Slim Shady" });
+
+            var album1Id = client.AddAlbum(new AddAlbumRequest()
+                { ArtistId = eminemId.Id, Genre = "Rap", Name = "Kamikaze", Year = 2018 });
+            Console.WriteLine($"Album1: {album1Id.Id}"); 
+
+            var album2Id = client.AddAlbum(new AddAlbumRequest()
+                { ArtistId = eminemId.Id, Genre = "Rap", Name = "Music to Be Murdered By", Year = 2020 });
+            Console.WriteLine($"Album2: {album2Id.Id}");
+
+            var count = client.CountAlbums(new CountAlbumsByArtistRequest() { ArtistId = eminemId.Id });
+            Console.WriteLine(count.Count);
+
+            var albums = client.GetAlbumsByArtist(new GetAlbumsByArtistRequest() { ArtistId = eminemId.Id });
+            Console.WriteLine(albums.Ids);
+        }
+    }
+}
---
+++ DistributedComputing/Lab8/Lab8_b.Server/Services/StudioService.cs
@@ -0,0 +1,79 @@
+using Grpc.Core;
+
+namespace Lab8_b.Server.Services
+{
+    public class StudioService : Studio.StudioBase
+    {
+        private readonly ILogger<StudioService> _logger;
+        private readonly Lab7_b.Studio _studio;
+        public StudioService(ILogger<StudioService> logger, Lab7_b.Studio studio)
+        {
+            _logger = logger;
+            _studio = studio;
+            _studio.Start(true);
+        }
+
+        public override Task<AddArtistReply> AddArtist(AddArtistRequest request, ServerCallContext context)
+        {
+            var id = _studio.AddArtist(request.Name);
+            return Task.FromResult(new AddArtistReply
+            {
+                Id = id.ToString()
+            });
+        }
+
+        public override Task<GetArtistsReply> GetArtists(GetArtistsRequest request, ServerCallContext context)
+        {
+            var artists = _studio.GetAllArtists().Select(a => a.Id);
+            return Task.FromResult(new GetArtistsReply
+            {
+                Ids = string.Join(',', artists)
+            });
+        }
+
+        public override Task<DeleteArtistReply> DeleteArtist(DeleteArtistRequest request, ServerCallContext context)
+        {
+            _studio.DeleteArtist(Guid.Parse(request.Id));
+            return Task.FromResult(new DeleteArtistReply
+            {
+                Message = "ok"
+            });
+        }
+
+        public override Task<UpdateArtistReply> UpdateArtist(UpdateArtistRequest request, ServerCallContext context)
+        {
+            _studio.UpdateArtist(Guid.Parse(request.Id), request.Name);
+            return Task.FromResult(new UpdateArtistReply
+            {
+                Message = "ok"
+            });
+        }
+
+        public override Task<AddAlbumReply> AddAlbum(AddAlbumRequest request, ServerCallContext context)
+        {
+            var id = _studio.AddAlbum(request.Name, request.Genre, request.Year, Guid.Parse(request.ArtistId));
+            return Task.FromResult(new AddAlbumReply
+            {
+                Id = id.ToString()
+            });
+        }
+
+        public override Task<CountAlbumsByArtistReply> CountAlbums(CountAlbumsByArtistRequest request, ServerCallContext context)
+        {
+            var count = _studio.CountAlbumsForArtist(Guid.Parse(request.ArtistId));
+            return Task.FromResult(new CountAlbumsByArtistReply
+            {
+                Count = count
+            });
+        }
+
+        public override Task<GetAlbumsByArtistReply> GetAlbumsByArtist(GetAlbumsByArtistRequest request, ServerCallContext context)
+        {
+            var albumIds = _studio.GetAlbumsForArtist(Guid.Parse(request.ArtistId)).Select(a => a.Id);
+            return Task.FromResult(new GetAlbumsByArtistReply
+            {
+                Ids = string.Join(',', albumIds)
+            });
+        }
+    }
+}
